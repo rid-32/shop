8a111c1eff90e12b91f7efd637640a18bc79e93e
Denis Rostokin <denis.rostokin@osinit.com>
2018-12-19 21:31:27 +0300

    Added products model validation

--- app/routes/products/handlers.js
+++ app/routes/products/handlers.js
@@ -2,22 +2,48 @@ import mongoose from 'mongoose'
 
 import Product from 'models/product'
 
+/* Получение всех продуктов из БД */
+const getProductsPayload = products => {
+    const total = products.length
+    const data = products.map(product => ({
+        _id: product._id,
+        name: product.name,
+        price: product.price,
+        request: {
+            type: 'GET',
+            url: `http://localhost:9090/products/${product._id}`,
+        },
+    }))
+
+    return {
+        total,
+        data,
+    }
+}
+
 export const getProducts = async (req, res) => {
     try {
         const products = await Product.find()
+            .select('name price _id')
+            .exec()
+        const payload = getProductsPayload(products)
 
-        res.status(200).json(products)
+        res.status(200).json(payload)
     } catch (error) {
         res.status(404).json('Not found')
     }
 }
 
-export const createProduct = async (req, res) => {
-    const product = new Product({
+/* Создание нового продукта */
+const createProductObject = config =>
+    new Product({
         _id: mongoose.Types.ObjectId(),
-        name: req.body.name,
-        price: req.body.price,
+        name: config.name,
+        price: config.price,
     })
+
+export const createProduct = async (req, res) => {
+    const product = createProductObject(req.body)
 
     try {
         const createdProduct = await product.save()
@@ -31,6 +57,7 @@ export const createProduct = async (req, res) => {
     }
 }
 
+/* Получение продукта по указанному id */
 export const getProduct = async (req, res) => {
     const id = req.params.productId
 
@@ -49,16 +76,22 @@ export const getProduct = async (req, res) => {
     }
 }
 
-export const changeProduct = async (req, res) => {
-    const productId = req.params.productId
-    const keys = Object.keys(req.body) || []
-    const newProductProps = keys.reduce(
+/* Изменение продукта по указанному id */
+const setNewProductProps = props => {
+    const keys = Object.keys(props) || []
+
+    return keys.reduce(
         (acc, key) =>
             key === 'name' || key === 'price'
-                ? { ...acc, [key]: req.body[key] }
+                ? { ...acc, [key]: props[key] }
                 : acc,
         {}
     )
+}
+
+export const changeProduct = async (req, res) => {
+    const productId = req.params.productId
+    const newProductProps = setNewProductProps(req.body)
 
     try {
         await Product.updateOne({ _id: productId }, newProductProps)
@@ -74,6 +107,7 @@ export const changeProduct = async (req, res) => {
     }
 }
 
+/* Удаление  продукта по id */
 export const deleteProduct = async (req, res) => {
     const productId = req.params.productId
 
